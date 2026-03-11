f1478d083f488ac3e1d6658dbe8fda6d438d1fd3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-03-11 14:41:17 -0400

    bump version to v0.2.0

--- phyloframe/__init__.py
+++ phyloframe/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Matthew Andres Moreno"
 __email__ = "m.more500@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 from ._auxlib import lazy_attach_stub
 
--- phyloframe/_auxlib/_get_phyloframe_version.py
+++ phyloframe/_auxlib/_get_phyloframe_version.py
@@ -1,2 +1,2 @@
 def get_phyloframe_version() -> str:
-    return "0.1.1"
+    return "0.2.0"
